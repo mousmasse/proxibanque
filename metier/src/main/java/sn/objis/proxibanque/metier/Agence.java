00c1d48e8291d6c5fb74eb6a27f5dbe4c05273d6
mousmasse <mousmasse@gmail.com>
2019-01-02 19:27:07 +0000

    Premier commit

---
+++ metier/src/main/java/sn/objis/proxibanque/metier/Agence.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package sn.objis.proxibanque.metier;
+
+/**
+ * @author mousmasse
+ *
+ */
+public class Agence {
+
+	/**
+	 * 
+	 */
+	public Agence() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
